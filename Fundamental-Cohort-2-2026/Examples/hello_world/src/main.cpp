0770b015574849f13a6647d78a449ec1c8645f73
KakadiyaSahil <kakadiyasahil9805@gmail.com>
2026-08-20 12:22:32 -0700

    Add hello world application and reorganize manifests

---
+++ Fundamental-Cohort-2-2026/Examples/hello_world/src/main.cpp
@@ -0,0 +1,22 @@
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+    int64_t previous_time = k_uptime_get();
+
+    while (1) {
+        int64_t current_time = k_uptime_get();
+        int64_t diff = current_time - previous_time;
+
+        printk("Hello World | Time: %lld.%03lld s | Diff: %lld ms\n",
+               current_time / 1000,
+               current_time % 1000,
+               diff);
+
+        previous_time = current_time;
+
+        k_sleep(K_SECONDS(1));
+    }
+
+    return 0;
+}
